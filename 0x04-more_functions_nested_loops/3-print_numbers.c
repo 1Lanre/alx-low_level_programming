eee8a8650a9e9e244fde549130f6c46076848293
1Lanre <ogunbiyilanre205@gmail.com>
2024-06-28 07:12:00 -0700

    print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0-9
+ * print_numbers - Print the numbers since 0 up to 9
  * Return: The numbers since 0 up to 9
  */
 
@@ -10,7 +10,7 @@ void print_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x=0; x<= 9; x++)
 	{
 	_putchar(x + '0');
 	}
