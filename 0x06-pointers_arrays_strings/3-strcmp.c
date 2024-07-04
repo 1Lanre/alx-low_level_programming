30b2e3beb759310467a9649989f7159a13acc0c4
1Lanre <ogunbiyilanre205@gmail.com>
2024-07-04 06:24:42 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s2:	A pointer to the second string to be compared.
  * Return: If str1 < str2, the negative difference of the first unmatched characters.
  * If str1 == str2, 0.
- * If str1 > str2, the positive difference of the first unmatched characters.
+ * If str1 > str2, the postive difference of the first unmatched characters.
  *
  */
 
@@ -20,3 +20,4 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (*s1 - *s2);
 }
+
